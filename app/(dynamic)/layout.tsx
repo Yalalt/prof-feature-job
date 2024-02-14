007b7799e5d1e0410f2aeba1c92bdcef789bcce4
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-15 01:10:33 +0800

    add session provider

---
+++ app/(dynamic)/layout.tsx
@@ -0,0 +1,10 @@
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { ReactNode } from 'react';
+import SessionProvider from '@/components/session-provider';
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const session = await getServerSession(authOptions);
+
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+}
