40c14e4cdc4718be2124d6872a07e264e21aafc2
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-15 01:10:56 +0800

    Add Restricted page

---
+++ app/(dynamic)/restricted/page.tsx
@@ -0,0 +1,16 @@
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function Page() {
+    const session = await getServerSession(authOptions);
+
+    if(!session) {
+        redirect("/");
+    }
+    return (
+        <div>
+            <h1>Restricted Page</h1>
+        </div>
+    )
+}
