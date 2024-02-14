384f1c5d8c2bf63998c4816500775843049eace8
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-15 01:12:00 +0800

    Add signin Button container

---
+++ app/(dynamic)/signin/page.tsx
@@ -0,0 +1,25 @@
+import SigninBtn from "@/components/signin-btn";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { getProviders } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+export default async function SignIn() {
+    const session = await getServerSession(authOptions);
+
+    if(session) {
+        redirect('/');
+    }
+
+    const providers: any = await getProviders();
+
+    return (
+        <>
+            {Object.values(providers).map((provider: any) => (
+                <div key={provider.name} className="flex w-screen h-screen justify-center items-center">
+                    <SigninBtn provider={provider} />
+                </div>
+            ))}
+        </>
+    )
+}
