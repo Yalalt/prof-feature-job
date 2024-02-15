725186952222651f0bfc304b9b9fcecb2cf5591b
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-15 18:56:48 +0800

    add types

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,6 @@
-import { users } from "./schema";
+import { skills, users, usersToSkills } from "./schema";
 
 export type NewUser = typeof users.$inferInsert;
 export type User = typeof users.$inferSelect;
+export type NewSkill = typeof skills.$inferInsert;
+export type NewUserToSkill = typeof usersToSkills.$inferInsert;
