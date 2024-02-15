a23d4740e0da1c568b305adc3e22fe8e76d3109a
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-15 10:24:19 +0800

    add AppShell theme components

---
+++ app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import SessionProvider from "@/components/session-provider";
+import AppShellContainer from "@/components/app-shell-container";
+
+export default async function Layout({children}:{children: ReactNode}) {
+    const session = await getServerSession(authOptions);
+
+    if(!session){
+        redirect('/signin');
+    }
+
+    return (
+        <SessionProvider session={session}>
+            <AppShellContainer user={session.user}>
+                {children}
+            </AppShellContainer>
+        </SessionProvider>
+    )
+}
---
+++ components/app-shell-container.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { AppShell, Avatar, Burger, Menu, NavLink } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconDashboard, IconDoorExit, IconSettings, IconStar, IconUser, IconUserCircle } from '@tabler/icons-react';
+import { signOut } from 'next-auth/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+export default function AppShellContainer({ children, user }: { children: React.ReactNode; user: any }) {
+  const [opened, { toggle }] = useDisclosure();
+  const pathname = usePathname();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding='md'
+    >
+      <AppShell.Header>
+        <div className='flex justify-between h-full'>
+          <div className='flex h-full items-center gap-5 p-5'>
+            <Burger opened={opened} onClick={toggle} hiddenFrom='sm' size='sm' />
+            Professional Network
+          </div>
+          <div className='flex justify-end gap-5 h-full items-center p-5'>
+            <div>Search</div>
+            <div>
+              <Menu>
+                <Menu.Target>
+                  <Avatar src={user.image} alt='user profile'>
+                    <IconUserCircle />
+                  </Avatar>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Label>Menu</Menu.Label>
+                  <Menu.Item leftSection={<IconSettings />}>
+                    <Link href='/dashboard/profile'>Edit Profile</Link>
+                  </Menu.Item>
+                  <Menu.Item leftSection={<IconDoorExit />}>
+                    <button onClick={() => signOut()}>Sign out</button>
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </div>
+          </div>
+        </div>
+      </AppShell.Header>
+      <AppShell.Navbar>
+        <NavLink
+          component={Link}
+          href='/dashboard'
+          label='Dashboard'
+          leftSection={<IconDashboard />}
+          active={pathname === '/dashboard'}
+        />
+        <NavLink
+          component={Link}
+          href='/dashboard/people'
+          label='People'
+          leftSection={<IconUser />}
+          active={pathname === '/dashboard/people'}
+        />
+        <NavLink
+          component={Link}
+          href='/dashboard/skills'
+          label='Skills'
+          leftSection={<IconStar />}
+          active={pathname === '/dashboard/skills'}
+        />
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
