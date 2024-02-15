24cbdcc7200ba43adac5ef3cd27ed7518067b3d7
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-16 01:09:39 +0800

    add user form

---
+++ components/user-form.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { UpdateUserState, updateUser } from '@/lib/actions';
+import { User } from '@/lib/types';
+import { Button, TextInput, Textarea } from '@mantine/core';
+import { useFormState } from 'react-dom';
+import { notifications } from '@mantine/notifications';
+import { useEffect, useState } from 'react';
+import Editor from './editor';
+
+export default function UserForm({ user }: { user: User }) {
+  const initialState: UpdateUserState = { errors: {} };
+
+  const [state, dispatch] = useFormState(updateUser, initialState);
+  const [bio, setBio] = useState(user.bio);
+
+  useEffect(() => {
+    if (state.success) {
+      notifications.show({
+        title: 'Success',
+        message: 'Profile has been updated',
+        color: 'green',
+      });
+    } else if (state.message) {
+      notifications.show({
+        title: 'Error',
+        message: 'The form submission seems to be invalid',
+        color: 'red',
+      });
+    }
+  }, [state]);
+
+  function handleUpdate(html: string) {
+    setBio(html);
+  }
+
+  return (
+    <div>
+      <form action={dispatch} className='flex flex-col gap-5 max-w-xl'>
+        <div>
+          <TextInput label='Job Title' name='jobTitle' error={state?.errors?.jobTitle} defaultValue={user.jobTitle!} />
+        </div>
+        <div>
+          <label>Bio</label>
+          <Editor content={user.bio || ''} onUpdate={handleUpdate} />
+          <input type='hidden' name='bio' value={bio || ''} />
+        </div>
+        <div>
+          <Button type='submit'>Submit</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
