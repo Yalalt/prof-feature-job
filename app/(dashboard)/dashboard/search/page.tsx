769bdb197249be70035b3febcd504b3037cffb24
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-15 18:57:19 +0800

    improve search query

--- app/(dashboard)/dashboard/search/page.tsx
+++ app/(dashboard)/dashboard/search/page.tsx
@@ -1,33 +1,43 @@
 import UsersTable from '@/components/users-table';
 import { db } from '@/lib/db';
 import { users } from '@/lib/schema';
-import { ilike, or } from 'drizzle-orm';
-import React from 'react'
+import { ilike, or, sql } from 'drizzle-orm';
+import React from 'react';
 
 interface IProps {
-    searchParams: { query: string }
+  searchParams: { query: string };
 }
 
 async function getUsers(query: string) {
-    const q = "%" + query + "%";
-    const res = await db.query.users.findMany({
-        where: or(
-            ilike(users.name, q),
-            ilike(users.jobTitle, q),
-            ilike(users.firstName, q),
-            ilike(users.lastName, q)
-        ),
-    });
-    return res;
+  const q = '%' + query + '%';
+  const res = await db.query.users.findMany({
+    where: or(ilike(users.name, q), ilike(users.jobTitle, q), ilike(users.firstName, q), ilike(users.lastName, q)),
+  });
+  return res;
+}
+
+async function fuzzySearch(query: string) {
+  const res = await db.select()
+    .from(users)
+    .where(
+        or(
+            sql`similarity(name, ${query}) > 0.2`,
+            sql`similarity(first_name, ${query}) > 0.2`,
+            sql`similarity(last_name, ${query}) > 0.2`,
+            sql`similarity(job_title, ${query}) > 0.2`
+        )
+    );
+
+  return res;
 }
 
 export default async function Page({ searchParams }: IProps) {
-    const { query } = searchParams;
-    const users = await getUsers(query);
+  const { query } = searchParams;
+  const users = await fuzzySearch(query);
 
   return (
     <div>
-        <UsersTable users={users} />
+      <UsersTable users={users} />
     </div>
-  )
+  );
 }
