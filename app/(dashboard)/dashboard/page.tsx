fb40ddab52ee3acd9981ddaacced7e4f89b6bceb
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-17 23:14:10 +0800

    add dashboard home

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,54 @@
-export default function Page(){
-    return (
-        <>
-        <h1>Dashboard he he</h1>
-        </>
-    )
+import { db } from '@/lib/db';
+import { skills, users, usersToSkills } from '@/lib/schema';
+import { count, desc, eq } from 'drizzle-orm';
+import { BarChart } from '@mantine/charts';
+
+async function getCountOfJobTitles() {
+  return await db
+    .select({
+      jobTitle: users.jobTitle,
+      count: count(),
+    })
+    .from(users)
+    .groupBy(users.jobTitle)
+    .orderBy(desc(count()));
+}
+
+async function getCountOfSkills() {
+    return await db
+      .select({ name: skills.name, count: count() })
+      .from(skills)
+      .leftJoin(usersToSkills, eq(skills.id, usersToSkills.skillId))
+      .groupBy(skills.name)
+      .orderBy(desc(count()));
+  }
+
+export default async function Page() {
+  const data = await getCountOfJobTitles();
+  const data2 = await getCountOfSkills();
+  return (
+    <div className="flex flex-col gap-5">
+      <h1>Dashboard</h1>
+      <h2 className='font-bold text-xl'>Job Titles</h2>
+      <BarChart
+        h={300}
+        data={data}
+        dataKey='jobTitle'
+        type='stacked'
+        orientation='vertical'
+        yAxisProps={{ width: 80 }}
+        series={[{ name: 'count', color: 'blue.6' }]}
+      />
+      <h2 className='font-bold text-xl'>Skill</h2>
+    <BarChart
+        h={300}
+        data={data2}
+        dataKey='name'
+        type='stacked'
+        orientation='vertical'
+        yAxisProps={{ width: 80 }}
+        series={[{ name: 'count', color: 'orange.6' }]}
+    />
+    </div>
+  );
 }
