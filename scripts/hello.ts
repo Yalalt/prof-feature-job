a3c8573c27123a58ed485ed349d1fa4b3512a89f
Yalalt Gandush <yalaltgandush@gmail.com>
2024-02-20 13:33:19 +0800

    migrate API route and protect route by secret key

---
+++ scripts/hello.ts
@@ -0,0 +1,7 @@
+export default function main() {
+    console.log("Hello world!");
+}
+
+if(require.main === module) {
+    main();
+}
---
+++ scripts/migrate.ts
@@ -0,0 +1,10 @@
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { db } from '@/lib/db';
+
+export default async function main() {
+    await migrate(db, { migrationsFolder: "drizzle" });
+}
+
+if (require.main === module) {
+    main();
+}
